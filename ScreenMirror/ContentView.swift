fca6c333f8bff207b6eaeb4aad2129fb85d38e12
Hdhvxud <jermainendifuna10@gmail.com>
2026-07-05 02:51:08 +0300

    Update ContentView.swift

--- ScreenMirror/ContentView.swift
+++ ScreenMirror/ContentView.swift
@@ -2,9 +2,9 @@ import SwiftUI
 import ReplayKit
 
 struct ContentView: View {
-    @AppStorage("android_ip", store: UserDefaults(suiteName: "group.com.yourname.ScreenMirror"))
+    @AppStorage("android_ip", store: UserDefaults(suiteName: "group.com.hdhvxud.ScreenMirror"))
     private var androidIP: String = "192.168.42.100"
-    @AppStorage("android_port", store: UserDefaults(suiteName: "group.com.yourname.ScreenMirror"))
+    @AppStorage("android_port", store: UserDefaults(suiteName: "group.com.hdhvxud.ScreenMirror"))
     private var androidPort: String = "12345"
     @State private var showPicker = false
 
